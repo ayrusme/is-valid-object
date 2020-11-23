45972b5a777f0a802b083ea005c3386c5418923a
Surya Raman <ayrusme@gmail.com>
2020-11-23 23:07:09 +0530

    INIT: Add base class

---
+++ README.md
@@ -0,0 +1,17 @@
+# Object-Schema
+
+object-schema is a Javascript library for making sure objects conform to a defined structure.
+
+## Installation
+<!-- TODO -->
+
+## Usage
+<!-- TODO -->
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ tests/test.js
@@ -0,0 +1,55 @@
+let toBeChecked = {
+    "first": "this is a string",
+    "second": 100,
+    "third": 50.52,
+    "fourth": {
+        "fifth": "hello string",
+        "sixth": {
+            "seventh": "doesn't matter"
+        }
+    },
+    "eighth": [
+        1234,
+        1234,
+        1234
+	],
+	"ninth": true
+};
+
+const schema = {
+    "first": {
+        "type": "string",
+        "length": 25
+    },
+    "second": {
+        "type": "integer",
+        "lower_range": 10,
+        "upper_range": 50
+    },
+    "third": {
+		"type": "float",
+		"lower_range": 10,
+        "upper_range": 50
+	},
+	"fourth": {
+		"type": "object",
+		"properties": {
+			"fifth": {
+				"type": "string"
+			},
+			"sixth": {
+				"type": "any"
+			}
+		}
+	},
+	"eighth": {
+		"type": "array",
+		"min_length": 2,
+		"max_length": 5,
+		"elements_type": "integer"
+	},
+	"ninth": {
+		"type": "bool"
+	}
+};
+
