667d608f54435f0c0ae1b8542dc5761e8cfff29e
Surya Raman <ayrusme@gmail.com>
2020-11-25 21:40:47 +0530

    DEV: Support for object checking, and prettier

--- tests/test.js
+++ tests/test.js
@@ -1,56 +1,57 @@
-const { ObjectChecker } = require('../lib/index');
+const { ObjectChecker } = require("../lib/index");
 
 const toBeChecked = {
-	first: 'this is a string',
-	second: 100,
-	third: 50.52,
-	fourth: {
-		fifth: 'hello string',
-		sixth: {
-			seventh: "doesn't matter",
-		},
-	},
-	eighth: [1234, 1234, 1234],
-	ninth: true,
+    first: "this is a string",
+    second: 100,
+    third: 50.52,
+    fourth: {
+        fifth: "hello string",
+        sixth: {
+            seventh: "doesn't matter",
+        },
+    },
+    eighth: [1234, 1234, 1234],
+    ninth: true,
 };
 
 const schema = {
-	first: {
-		type: 'string',
-		length: 25,
-	},
-	second: {
-		type: 'integer',
-		lower_range: 10,
-		upper_range: 50,
-	},
-	third: {
-		type: 'float',
-		lower_range: 10,
-		upper_range: 50,
-	},
-	fourth: {
-		type: 'object',
-		properties: {
-			fifth: {
-				type: 'string',
-			},
-			sixth: {
-				type: 'any',
-			},
-		},
-	},
-	eighth: {
-		type: 'array',
-		min_length: 2,
-		max_length: 5,
-		elements_type: 'integer',
-	},
-	ninth: {
-		type: 'bool',
-	},
+    first: {
+        type: "string",
+        length: 25,
+    },
+    second: {
+        type: "integer",
+        lower_range: 10,
+        upper_range: 50,
+    },
+    third: {
+        type: "float",
+        lower_range: 10,
+        upper_range: 50,
+    },
+    fourth: {
+        type: "object",
+        properties: {
+            fifth: {
+                type: "string",
+            },
+            sixth: {
+                type: "any",
+            },
+        },
+    },
+    eighth: {
+        type: "array",
+        min_length: 2,
+        max_length: 5,
+        elements_type: "integer",
+    },
+    ninth: {
+        type: "bool",
+    },
 };
 
+console.time('object-checker')
 const checker = new ObjectChecker();
-
 checker.check(toBeChecked, schema);
+console.timeEnd('object-checker')
