457f0cbbf36786bab636399ba911bf209f95b461
Surya Raman <ayrusme@gmail.com>
2020-11-27 11:21:18 +0530

    test: add bool

---
+++ test/boolean.js
@@ -0,0 +1,57 @@
+import { doesNotThrow, throws } from "assert";
+
+import { ObjectChecker } from "../lib/index.js";
+
+describe("Boolean", function () {
+    it("should not throw when presented with valid data type", () => {
+        doesNotThrow(() => {
+            const checker = new ObjectChecker();
+            checker.check(
+                {
+                    bool_key: false,
+                },
+                {
+                    bool_key: {
+                        type: "bool",
+                    },
+                }
+            );
+        });
+    });
+    it("should throw when presented with invalid data type", () => {
+        throws(() => {
+            const checker = new ObjectChecker();
+            checker.check(
+                {
+                    bool_key: [1234, 1234, "1234"],
+                },
+                {
+                    bool_key: {
+                        type: "bool",
+                    },
+                }
+            );
+        });
+    });
+    it("should throw when presented with no/invalid schema", () => {
+        // no schema
+        throws(() => {
+            const checker = new ObjectChecker();
+            checker.check({
+                bool_key: true,
+            });
+        });
+        // invalid schema
+        throws(() => {
+            const checker = new ObjectChecker();
+            checker.check(
+                {
+                    bool_key: true,
+                },
+                {
+                    bool_key: "something",
+                }
+            );
+        });
+    });
+});
